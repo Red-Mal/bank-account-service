6a09c3490cbc7ea96bd0ae2b9b97f6006ffd3cb1
redae <reda.elmallahi@gmail.com>
2022-10-27 15:31:01 +0100

    graphql commit

---
+++ src/main/java/org/sid/bankaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,70 @@
+package org.sid.bankaccountservice.web;
+
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.entities.Customer;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.sid.bankaccountservice.repositories.CustomerRepository;
+import org.sid.bankaccountservice.service.AccountService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+
+    private BankAccountRepository bankAccountRepository;
+    private AccountService accountService;
+    private CustomerRepository customerRepository;
+
+    public BankAccountGraphQLController(BankAccountRepository bankAccountRepository, AccountService accountService, CustomerRepository customerRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+        this.accountService = accountService;
+
+        this.customerRepository = customerRepository;
+    }
+
+    @QueryMapping
+    public List<BankAccount> accountList(){
+        return bankAccountRepository.findAll();
+    }
+
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("Account %s not Found",id)));
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO updateAccount(@Argument String id, @Argument BankAccountRequestDTO bankAccount){
+
+        return accountService.updateAccount(id,bankAccount);
+    }
+
+    @MutationMapping
+    public Boolean deleteAccount(@Argument String id){
+
+        accountService.deleteAccount(id);
+        return true;
+    }
+
+    @QueryMapping
+    public List<Customer> customers(){
+        return customerRepository.findAll();
+    }
+}
+
+
+/*
+record BankAccountDTO(Double balance,String type,String currency){
+
+}*/
